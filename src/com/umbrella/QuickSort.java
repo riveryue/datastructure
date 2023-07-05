4976f41eb6b8b9abea8ad5fca4ca86f53c1602e7
yao <summerpear@foxmail.com>
2023-07-05 23:33:39 +0800

    快速排序

---
+++ src/com/umbrella/QuickSort.java
@@ -0,0 +1,41 @@
+package com.umbrella;
+
+import java.util.Arrays;
+
+/**
+ * 快速排序之双指针实现
+ */
+public class QuickSort {
+    private static void quickSort(int[] arr, int left, int right) {
+        if (left >= right) {
+            return;
+        }
+        int pivot = arr[left];
+        int i = left;
+        int j = right;
+        while (i < j) {
+            //从左指针开始找到第一个大于基准值的元素对应的下标
+            while (arr[i] < pivot) {
+                i++;
+            }
+            //从右指针开始找到第一个小于基准值的元素对应的下标
+            while (arr[j] > pivot) {
+                j--;
+            }
+            if (i < j) {
+                int tmp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = tmp;
+            }
+        }
+        quickSort(arr, left, j);
+        quickSort(arr, j + 1, right);
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {30, 40, 60, 10, 20, 50};
+        System.out.println(Arrays.toString(arr));
+        quickSort(arr, 0, arr.length - 1);
+        System.out.println(Arrays.toString(arr));
+    }
+}
